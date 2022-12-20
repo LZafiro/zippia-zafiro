01fabcfdb5f087eeea244d1405b7a1e9e2ab1321
LZafiro <zafiro.unifesp@gmail.com>
2022-12-20 14:23:08 -0300

    Feature v1

--- pages/test/jobs/index.tsx
+++ pages/test/jobs/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GetServerSideProps } from "next";
 import styles from "../../../styles/jobsPage.module.css";
 import Card from "../../../components/card";
@@ -13,11 +13,65 @@ export interface Job {
 }
 
 export default function Jobs({ jobs }: { jobs: Job[] }): JSX.Element {
+  // We store the jobs in a state variable to be able to filter them
+  const [jobList, setJobList] = useState<Job[]>(jobs);
+  const [filterLast7Days, setFilterLast7Days] = useState<boolean>(false);
+  const [companyNameFilter, setCompanyNameFilter] = useState<string>("");
+
+  useEffect(() => {
+    if (filterLast7Days) {
+      setCompanyNameFilter("");
+      // We filter the jobs by last 7 days
+      const filteredJobs = jobs.filter((job) => {
+        const postingDate = new Date(job.postingDate);
+        const today = new Date();
+        const diffTime = Math.abs(today.getTime() - postingDate.getTime());
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return diffDays <= 7;
+      });
+      setJobList(filteredJobs);
+    } else {
+      setJobList(jobs);
+    }
+  }, [filterLast7Days]);
+
+  useEffect(() => {
+    if (companyNameFilter) {
+      setFilterLast7Days(false);
+      const filteredData = jobs.filter((value) =>
+        value.companyName
+          .toLowerCase()
+          .includes(companyNameFilter.toLowerCase())
+      );
+      setJobList(filteredData);
+    } else {
+      setJobList(jobs);
+    }
+  }, [companyNameFilter]);
+
   return (
     <div className={styles.wrapper}>
+      <div className={styles.actionButtonsContainer}>
+        <input
+          className={styles.companyNameSearch}
+          placeholder="Filter by company name"
+          value={companyNameFilter}
+          onChange={(e) => setCompanyNameFilter(e.target.value)}
+        />
+        <div
+          onClick={() => setFilterLast7Days((prev) => !prev)}
+          className={
+            filterLast7Days
+              ? styles.customCheckboxContainerSelected
+              : styles.customCheckboxContainer
+          }
+        >
+          Filter by last 7 days
+        </div>
+      </div>
       <div className={styles.cardsContainer}>
         {/** We only want to display the first 10 jobs */}
-        {jobs.slice(0, 10).map((job: Job, index: number) => (
+        {jobList.slice(0, 10).map((job: Job, index: number) => (
           <Card key={index} job={job} />
         ))}
       </div>
